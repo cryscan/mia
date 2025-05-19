0762a2a50f604d02a1c1119f1a11ce1b3b98f548
cryscan <cryscan@umich.edu>
2025-05-20 02:07:30 +0800

    Rework device.

--- src/loom/tensor.rs
+++ src/loom/tensor.rs
@@ -1,6 +1,5 @@
 use std::{marker::PhantomData, sync::Arc};
 
-use derive_more::{Deref, DerefMut};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
@@ -28,79 +27,29 @@ pub enum TensorError {
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct TensorId;
 
+/// A statically typed tensor. Good to fit into typed APIs.
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct TensorUntyped<D> {
+pub struct Tensor<D, T> {
     device: Arc<D>,
     layout: Layout,
-    r#type: DataType,
     id: uid::Id<TensorId>,
+    phantom: PhantomData<T>,
 }
 
-impl<D: Device> TensorUntyped<D> {
+impl<D: Device, T: Scalar> Tensor<D, T> {
+    #[inline]
+    pub fn device(&self) -> &D {
+        &self.device
+    }
+
     #[inline]
     pub fn layout(&self) -> Layout {
         self.layout.clone()
     }
 
     #[inline]
-    pub fn data_type(&self) -> DataType {
-        self.r#type
-    }
-
-    #[inline]
-    pub fn data_count(&self) -> usize {
-        self.layout.size() * self.r#type.count()
-    }
-
-    #[inline]
-    pub fn data_size(&self) -> usize {
-        self.layout.size() * self.r#type.size()
-    }
-
-    #[inline]
     pub fn id(&self) -> uid::Id<TensorId> {
         self.id
-    }
-
-    /// Converts the untyped type to a typed one. Returns error if type mismatches.
-    #[inline]
-    pub fn try_into_typed<T: Scalar>(self) -> Result<Tensor<D, T>, TensorError> {
-        if self.r#type != T::DATA_TYPE {
-            return Err(TensorError::Type(self.r#type, T::DATA_TYPE));
-        }
-        Ok(Tensor {
-            tensor: self,
-            phantom: PhantomData,
-        })
-    }
-
-    #[inline]
-    pub fn ref_count(&self) -> usize {
-        Arc::strong_count(&self.device)
-    }
-}
-
-impl<D: Device + Clone> TensorUntyped<D> {
-    #[inline]
-    pub fn device(&self) -> D {
-        self.device.as_ref().clone()
-    }
-}
-
-/// A statically typed tensor. Good to fit into typed APIs.
-#[derive(Debug, Clone, PartialEq, Eq, Deref, DerefMut)]
-pub struct Tensor<D, T> {
-    #[deref]
-    #[deref_mut]
-    tensor: TensorUntyped<D>,
-    phantom: PhantomData<T>,
-}
-
-impl<D: Device, T: Scalar> Tensor<D, T> {
-    /// Transform the tensor into an untyped one.
-    #[inline]
-    pub fn into_untyped(self) -> TensorUntyped<D> {
-        self.tensor
     }
 
     #[inline]
@@ -118,7 +67,7 @@ impl<D: Device, T: Scalar> Tensor<D, T> {
     pub fn reshape(mut self, layout: impl IntoLayout) -> Result<Self, TensorError> {
         let layout = layout.into_layout();
         if self.layout.size() != layout.size() {
-            return Err(TensorError::Reshape(self.layout(), layout));
+            return Err(TensorError::Reshape(self.layout, layout));
         }
         self.layout = layout;
         Ok(self)
@@ -132,16 +81,14 @@ impl<D: Device, T: Scalar> Tensor<D, T> {
         if self.data_size() != size {
             return Err(TensorError::Cast(self.data_size(), size));
         }
-        let TensorUntyped { device, id, .. } = self.tensor;
-        let r#type = U::DATA_TYPE;
-        let tensor = TensorUntyped {
+        let Self { device, id, .. } = self;
+        let phantom = PhantomData;
+        Ok(Tensor {
             device,
             layout,
-            r#type,
             id,
-        };
-        let phantom = PhantomData;
-        Ok(Tensor { tensor, phantom })
+            phantom,
+        })
     }
 }
 
@@ -151,32 +98,28 @@ impl<D: Device + Clone, T: Scalar> Tensor<D, T> {
     pub fn zeros(device: &D, layout: impl IntoLayout) -> Self {
         let device = Arc::new(device.clone());
         let layout = layout.into_layout();
-        let r#type = T::DATA_TYPE;
         let id = uid::Id::new();
         let phantom = PhantomData;
-        let tensor = TensorUntyped {
+        Self {
             device,
             layout,
-            r#type,
             id,
-        };
-        Self { tensor, phantom }
+            phantom,
+        }
     }
 
     /// Create a tensor of zeros from current device and layout.
     #[inline]
     pub fn zeros_like(&self) -> Self {
-        let device = self.device().into();
+        let device = self.device.as_ref().clone().into();
         let layout = self.layout();
         let id = uid::Id::new();
-        let r#type = self.r#type;
-        let tensor = TensorUntyped {
+        let phantom = PhantomData;
+        Self {
             device,
             layout,
-            r#type,
             id,
-        };
-        let phantom = PhantomData;
-        Self { tensor, phantom }
+            phantom,
+        }
     }
 }
